bb5c2873991aae6f4208b391eadb3d6850537bd3
osirisOfGit <github.com.j95fw@passmail.net>
2024-07-03 18:50:15 -0500

    Halfway to decent

--- checkers/src/main.rs
+++ checkers/src/main.rs
@@ -1,21 +1,26 @@
-use std::path::Path;
+use std::{ops::{Add, Div, Mul}, path::Path, thread::sleep, time::{self, Duration}};
 
 use bevy::{
     app::{App, Startup},
     asset::{AssetPath, AssetServer},
     log::error,
-    math::Rect,
-    prelude::{Camera2dBundle, Commands, Query, Res},
+    math::{Rect, Vec3},
+    prelude::{Camera2dBundle, Commands, DefaultPlugins, PluginGroup, Query, Res, WindowPlugin, default},
     sprite::{Sprite, SpriteBundle},
     transform::components::Transform,
-    window::Window,
-    DefaultPlugins,
+    window::{Window, WindowResolution},
 };
 use tiled::{LayerType, Loader};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WindowResolution::new(128., 128.).with_scale_factor_override(4.0),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, setup)
         .run();
 }
@@ -32,26 +37,38 @@ fn setup(mut commands: Commands, windows: Query<&Window>, asset_server: Res<Asse
                 _ => panic!("Layer #0 is not a tile layer"),
             };
 
+            let resolution = &windows.single().resolution;
+            let top_left_coord = (
+                0. - resolution.width().div(2.),
+                0. - resolution.height().div(2.),
+            );
+
             for x in 0..layer.width().unwrap() {
                 for y in 0..layer.height().unwrap() {
                     layer.get_tile(x as i32, y as i32).map(|layer_tile| {
                         layer_tile.get_tile().map(|tile| {
-                            let image_path =
-                                tile.tileset().image.as_ref().unwrap().source.file_name().unwrap();
                             commands.spawn(SpriteBundle {
                                 sprite: Sprite {
                                     rect: Some(Rect::new(
-                                        (layer_tile.id() * layer_tile.get_tileset().tile_width) as f32,
                                         0.,
-                                        ((layer_tile.id() + 1) * layer_tile.get_tileset().tile_width) as f32,
+                                        0.,
+                                        layer_tile.get_tileset().tile_width as f32,
                                         layer_tile.get_tileset().tile_height as f32,
                                     )),
                                     ..Default::default()
                                 },
-                                texture: asset_server.load(AssetPath::from_path(Path::new(image_path))),
+                                texture: asset_server.load(AssetPath::from_path(Path::new(
+                                    tile.tileset()
+                                        .image
+                                        .as_ref()
+                                        .unwrap()
+                                        .source
+                                        .file_name()
+                                        .unwrap(),
+                                ))),
                                 transform: Transform::from_xyz(
-                                    (x * map.tile_width) as f32,
-                                    (y * map.tile_height) as f32,
+                                    (top_left_coord.0 + map.tile_width.mul(x) as f32),
+                                    (top_left_coord.1 + map.tile_height.mul(y) as f32),
                                     0.,
                                 ),
                                 ..Default::default()
