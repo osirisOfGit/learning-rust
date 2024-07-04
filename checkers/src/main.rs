60e33833603bfb72ab9504526d1a6e995a156022
osirisOfGit <github.com.j95fw@passmail.net>
2024-07-03 19:53:50 -0500

    Now we're in business!

--- checkers/src/main.rs
+++ checkers/src/main.rs
@@ -1,11 +1,18 @@
-use std::{ops::{Add, Div, Mul}, path::Path, thread::sleep, time::{self, Duration}};
+use std::{
+    ops::{Add, Div, Mul},
+    path::Path,
+    thread::sleep,
+    time::{self, Duration},
+};
 
 use bevy::{
     app::{App, Startup},
     asset::{AssetPath, AssetServer},
-    log::error,
+    log::{error, info},
     math::{Rect, Vec3},
-    prelude::{Camera2dBundle, Commands, DefaultPlugins, PluginGroup, Query, Res, WindowPlugin, default},
+    prelude::{
+        default, Camera2dBundle, Commands, DefaultPlugins, PluginGroup, Query, Res, WindowPlugin,
+    },
     sprite::{Sprite, SpriteBundle},
     transform::components::Transform,
     window::{Window, WindowResolution},
@@ -39,8 +46,8 @@ fn setup(mut commands: Commands, windows: Query<&Window>, asset_server: Res<Asse
 
             let resolution = &windows.single().resolution;
             let top_left_coord = (
-                0. - resolution.width().div(2.),
-                0. - resolution.height().div(2.),
+                (0. - resolution.width().div(2.)) + map.tile_width.div(2) as f32,
+                (0. - resolution.height().div(2.)) + map.tile_height.div(2) as f32,
             );
 
             for x in 0..layer.width().unwrap() {
@@ -50,10 +57,10 @@ fn setup(mut commands: Commands, windows: Query<&Window>, asset_server: Res<Asse
                             commands.spawn(SpriteBundle {
                                 sprite: Sprite {
                                     rect: Some(Rect::new(
+                                        (0 + tile.tileset().tile_width.mul(layer_tile.id())) as f32,
                                         0.,
-                                        0.,
-                                        layer_tile.get_tileset().tile_width as f32,
-                                        layer_tile.get_tileset().tile_height as f32,
+                                        tile.tileset().tile_width.mul(layer_tile.id() + 1) as f32,
+                                        tile.tileset().tile_height as f32,
                                     )),
                                     ..Default::default()
                                 },
@@ -67,8 +74,8 @@ fn setup(mut commands: Commands, windows: Query<&Window>, asset_server: Res<Asse
                                         .unwrap(),
                                 ))),
                                 transform: Transform::from_xyz(
-                                    (top_left_coord.0 + map.tile_width.mul(x) as f32),
-                                    (top_left_coord.1 + map.tile_height.mul(y) as f32),
+                                    top_left_coord.0 + map.tile_width.mul(x) as f32,
+                                    top_left_coord.1 + map.tile_height.mul(y) as f32,
                                     0.,
                                 ),
                                 ..Default::default()
